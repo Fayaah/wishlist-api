9068688cf60f0a8adf1ef4ef0da22f345709b835
Louise Baulan <louise@yaggo.co>
2022-02-03 11:52:20 +0100

    (zero) : first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("custom-logger").config({level: 0});
+
+// ================== set up ==================>
+const app = express();
+const port = process.env.PORT || 8080;
+
+// ================== middleware ==================>
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({origin: "http://localhost:8081"}));
+
+app.get("/", (req, res) => {
+    res.json({message: "Welcome to this app !"});
+})
+
+// ================== starting server ==================>
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
